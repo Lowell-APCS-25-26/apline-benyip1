e4a4cc8cc1d8d59024946a5b485d17c13d81b82f
benjamin yip <benjaminyip1@gmail.com>
2026-04-20 17:08:21 +0000

    done?

--- lib/src/main/java/org/APCSLowell/APLine.java
+++ lib/src/main/java/org/APCSLowell/APLine.java
@@ -1,12 +1,16 @@
 package org.APCSLowell;
 
 public class APLine {
-    int a, b, c;
-    public APLine(int a, int b, int c) {}
+    private int a, b, c;
+    public APLine(int givenA, int givenB, int givenC) {
+        a = givenA;
+        b = givenB;
+        c = givenC;
+    }
     public boolean isOnLine(int x, int y) {
         return a * x + b * y + c == 0;
     }
     public double getSlope() {
-        return -(double)a / (double)b;
+        return (double)-a / (double)b;
     }
 }
